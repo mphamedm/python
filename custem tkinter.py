4d40074d01aa84ef6009d6486774d30fca1d7c02
mphamedm <104618250+mphamedm@users.noreply.github.com>
2023-10-13 02:41:04 +0300

    Add files via upload

---
+++ custem tkinter.py
@@ -0,0 +1,20 @@
+import customtkinter
+
+customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
+customtkinter.set_default_color_theme("green")  # Themes: blue (default), dark-blue, green
+
+app = customtkinter.CTk()  # create CTk window like you do with the Tk window
+app.geometry("500x500")
+
+def button_function():
+    entry.insert (0, "enter yoyr name")
+    
+
+# Use CTkButton instead of tkinter Button
+button = customtkinter.CTkButton(master=app, text="CTkButton", command=button_function)
+button.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
+
+entry= customtkinter.CTkEntry(app)
+entry.pack()
+
+app.mainloop()
